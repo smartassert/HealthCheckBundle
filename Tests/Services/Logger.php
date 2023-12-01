16d48890e2ce04eecead7cd90a2d7a49f19a1ff6
Jon Cram <webignition@gmail.com>
2023-12-01 12:15:02 +0000

    Use php-cs-fixer 3.38.2 (#52)

--- Tests/Services/Logger.php
+++ Tests/Services/Logger.php
@@ -8,39 +8,39 @@ use Psr\Log\LoggerInterface;
 
 class Logger implements LoggerInterface
 {
-    public function emergency(\Stringable|string $message, array $context = []): void
+    public function emergency(string|\Stringable $message, array $context = []): void
     {
     }
 
-    public function alert(\Stringable|string $message, array $context = []): void
+    public function alert(string|\Stringable $message, array $context = []): void
     {
     }
 
-    public function critical(\Stringable|string $message, array $context = []): void
+    public function critical(string|\Stringable $message, array $context = []): void
     {
     }
 
-    public function error(\Stringable|string $message, array $context = []): void
+    public function error(string|\Stringable $message, array $context = []): void
     {
     }
 
-    public function warning(\Stringable|string $message, array $context = []): void
+    public function warning(string|\Stringable $message, array $context = []): void
     {
     }
 
-    public function notice(\Stringable|string $message, array $context = []): void
+    public function notice(string|\Stringable $message, array $context = []): void
     {
     }
 
-    public function info(\Stringable|string $message, array $context = []): void
+    public function info(string|\Stringable $message, array $context = []): void
     {
     }
 
-    public function debug(\Stringable|string $message, array $context = []): void
+    public function debug(string|\Stringable $message, array $context = []): void
     {
     }
 
-    public function log($level, \Stringable|string $message, array $context = []): void
+    public function log($level, string|\Stringable $message, array $context = []): void
     {
     }
 }
